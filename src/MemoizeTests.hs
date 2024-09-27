3d8657b548c5479079fc338a02422ba8ff321bfb
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2024-09-27 20:31:10 +0200

    File for testing memoization strategies

---
+++ src/MemoizeTests.hs
@@ -0,0 +1,5 @@
+module MemoizeTests (myDeriveMemoize) where
+import           Data.Function.Memoize (Memoizable (memoize))
+
+myDeriveMemoize :: Memoizable b => (a -> b) -> (b -> a) -> (a -> v) -> a -> v
+myDeriveMemoize deconstruct construct f = memoize (f . construct) . deconstruct
